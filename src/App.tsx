558373c8063fe0a0be809f57b6abb2d44443d964
Aida Chernyavskaya <aidachernyavskaya@gmail.com>
2023-06-29 16:20:32 +0300

    get list of users using axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Card, {CardVariant} from "./components/Card";
 import UserList from "./components/UserList";
 import {IUser} from "./types/types";
+import axios from "axios";
 
 const App = () => {
-    const users: IUser[] = [
-        {id: 1, name: 'Aida', email: 'a@ma.ru', address: {city: 'Spb', street: 'dubochki', zipcode: '23233'}},
-        {id: 2, name: 'Vitaly', email: 'sewe@mail.ru', address: {city: 'Spb', street: 'parnas', zipcode: '232345'}},
-    ]
+    const [users, setUsers] = useState<IUser[]>([]);
+
+    useEffect(() => {
+        fetchUsers();
+    }, [])
+
+    async function fetchUsers() {
+        try{
+            const response = await axios.get<IUser[]>('https://jsonplaceholder.typicode.com/users');
+            setUsers(response.data);
+        } catch (e){
+            alert(e);
+        }
+    }
+
     return (
         <div>
             <Card onClick={(num) => console.log('click', num)} variant={CardVariant.outlined} width={'200px'} height={'200px'}>
