3671775bda7340bac88564684ecbfa238bb63227
Aida Chernyavskaya <aidachernyavskaya@gmail.com>
2023-06-29 15:47:35 +0300

    UserList component added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import Card, {CardVariant} from "./components/Card";
+import UserList from "./components/UserList";
+import {IUser} from "./types/types";
 
 const App = () => {
+    const users: IUser[] = [
+        {id: 1, name: 'Aida', email: 'a@ma.ru', address: {city: 'Spb', street: 'dubochki', zipcode: '23233'}},
+        {id: 2, name: 'Vitaly', email: 'sewe@mail.ru', address: {city: 'Spb', street: 'parnas', zipcode: '232345'}},
+    ]
     return (
         <div>
-          <Card onClick={(num) => console.log('click', num)} variant={CardVariant.outlined} width={'200px'} height={'200px'}>
-              <button>Нажми меня</button>
-          </Card>
+            <Card onClick={(num) => console.log('click', num)} variant={CardVariant.outlined} width={'200px'} height={'200px'}>
+                <button>Нажми меня</button>
+            </Card>
+            <UserList users={users}/>
         </div>
     );
 };
