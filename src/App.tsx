e1d6852c6a1cbb71ae7128ede40e571f146f9c26
Aida Chernyavskaya <aidachernyavskaya@gmail.com>
2023-06-29 14:42:48 +0300

    Functional component added

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import Card, {CardVariant} from "./components/Card";
 
 const App = () => {
     return (
         <div>
-          Work!
+          <Card onClick={(num) => console.log('click', num)} variant={CardVariant.outlined} width={'200px'} height={'200px'}>
+              <button>Нажми меня</button>
+          </Card>
         </div>
     );
 };
